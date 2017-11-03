d0acd74c13d6ab98f669a16e08c4fe6015560a2b
behinger <behinger@uos.de>
2017-11-04 00:29:57 +0100

    gramm init

--- init_agittarget.m
+++ init_agittarget.m
@@ -1,5 +1,15 @@
-projectFolder = fileparts(which('init_agittarget.m'));
-cd(projectFolder)
+gitpath = fileparts(which('init_agittarget.m'));
+cd(gitpath)
 
-addpath('./edfread/build/linux64');
-addpath('./edfread/build/win32');
+addpath('./lib/edfread/build/linux64');
+addpath('./lib/edfread/build/win32');
+addpath('./Analysis/')
+addpath('./lib/gramm/')
+
+datapath = '/net/store/nbp/projects/agittarget/';
+if IsWin && strcmpi(getenv('computername'),'ANANSI')
+    % Bene's laptop
+    % I use sftp net drive to mount a remote folder on drive "N:"
+    datapath = fullfile(['n:' datapath]);
+
+end
